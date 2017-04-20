959c11279fc7a949d077e9f45aa0675e58fe849b
Brad <brad@exp.org.au>
2017-04-20 11:33:58 +1000

    Initial commit

---
+++ Assets/ActivateScreens.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateScreens : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Check the number of monitors connected.
+        if (Display.displays.Length > 1)
+            // Activate the display 1 (second monitor connected to the system).
+            Display.displays[1].Activate();        
+    }
+    
+}
